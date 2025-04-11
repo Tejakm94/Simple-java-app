3cff0e6f1e3dc13d3a3ada6db7ddb74af17f3884
Ubuntu <ubuntu@ip-172-31-92-188.ec2.internal>
2025-04-11 05:19:00 +0000

    latest commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use an official OpenJDK base image
+FROM openjdk:17-jdk-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the Java source code into the container
+COPY HelloWorldServer.java .
+
+# Compile the Java program
+RUN javac HelloWorldServer.java
+
+# Expose port 8080
+EXPOSE 8080
+
+# Run the Java program
+CMD ["java", "HelloWorldServer"]
+
---
+++ simple-java-project/HelloWorldServer.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+public class HelloWorldServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        System.out.println("Server started on port 8080");
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello, World from Java!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
+
